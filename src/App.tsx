c8c265af608daecdda5a80be0a6d2eeb28df5e9b
Mike Healy <design@mikehealy.com.au>
2024-09-22 08:50:53 +1000

    Initial commit. Vite, React, Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='px-4 lg:px-12'>
+      <h1>Then When</h1>
+      <p className='p-4 bg-orange-200'>Easily add time</p>
+    </div>
+  )
+}
+
+export default App
