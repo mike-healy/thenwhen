06d42ed52e484167a00d3ca3ca58910a2f417e31
Mike Healy <design@mikehealy.com.au>
2024-09-30 21:07:09 +1000

    Show modifiers calculated to minutes (no date maths yet)

--- src/components/Chain.tsx
+++ src/components/Chain.tsx
@@ -1,11 +1,11 @@
 import React, { useRef, useState } from 'react'
-import { formatTime } from '../shared/functions.js'
+import { formatTime, stringToMinutes } from '../shared/functions.js'
 
 export default () => {
     const newModifierRef = useRef(null);
 
     const [start, setStart] = useState(new Date());
-    const [modifiers, setModifiers] = useState([]);
+    const [modifiers, setModifiers] = useState([]); // '1:00', '2:30', '-0:15'
 
     const addModifier = (value: string) => {
       setModifiers([...modifiers, value])
@@ -17,7 +17,7 @@ export default () => {
 
     const changeModifier = (value: string, index: number) => {}
 
-    return <main className="order-1 bg-slate-100 p-2 rounded-md">
+    return <main className="order-1 bg-slate-100 p-2 rounded-md max-w-96">
       <div>
         <header className='p-6 bg-slate-900 text-brand-orange rounded-t'>
           {formatTime(start)} <span className="opacity-70 ml-4">(placeholder)</span>
@@ -25,14 +25,14 @@ export default () => {
 
         {modifiers.length > 0 && (
             <>
-              <small className='block py-2 text-center'>(nothing happens yet)</small>
               <ul className='mb-6'>
                 {modifiers.map((mod, index) => (
                   <li
                   key={index}
-                  className='px-6 py-2 even:bg-slate-200 flex items-center justify-between'
+                  className='px-6 py-2 even:bg-slate-200 flex items-center gap-x-4 justify-between'
                   >
                     <span>{mod}</span>
+                    <span>{`${stringToMinutes(mod) < 0 ? 'sub' : 'add'} ${stringToMinutes(mod)}`}</span> {/* reducer? */}
                     <button
                       onClick={() => removeModifier(index)}
                       className='font-bold p-2'
