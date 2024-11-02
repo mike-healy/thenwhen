ed0cd3f43757787d08b9244b1ac32b3b92e19805
Mike Healy <design@mikehealy.com.au>
2024-11-02 10:20:11 +1000

    Add OG social share image and link to Twitter in credit

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,18 @@ function App() {
           </p>
         </section>
       </div>
-      <footer className="py-4 md:py-8 text-sm text-slate-400">
-        by <a href="https://www.mikehealy.com.au" className='text-blue-400 decoration-blue-600 underline'>Mike Healy</a>
+      <footer className="flex items-center gap-x-4 py-4 md:py-8 text-sm text-slate-400">
+        <span>by <a href="https://www.mikehealy.com.au" className='text-blue-400 decoration-blue-600 underline'>Mike Healy</a></span>
+        <a
+          href="https://x.com/mike_hasarms"
+          target="_blank"
+          rel="no-opener"
+          className="text-slate-200"
+        >
+          <svg className="inline-block" role="img" aria-label="Twitter X" width="15.6" height="16" viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill="currentColor" d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"></path>
+          </svg>
+        </a>
       </footer>
     </div>
   )
