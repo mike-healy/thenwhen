084fa9e7f4d00e19ff01e6713b0a9d25bb6575a0
Mike Healy <design@mikehealy.com.au>
2025-06-13 11:49:36 +1000

    Add PostHog web tracking. Envs come from Vercel setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { PostHogProvider } from 'posthog-js/react'
 import './index.css'
+
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
